1948f2e7afec9b7d2b5d5da8ae64344f9d5e923d
Lars Haalck <lars.haalck@uni-muenster.de>
2022-01-08 16:24:46 +0100

    Add stdin and file loading

--- src/clip.rs
+++ src/clip.rs
@@ -3,15 +3,35 @@ use serde::{Deserialize, Serialize};
 use serde_json;
 use std::collections::HashMap;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::io::Read;
+use std::path::PathBuf;
+
+pub type ClipKey = char;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Clip(pub String);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Clips {
-    pub named: HashMap<String, Clip>,
+    pub named: HashMap<ClipKey, Clip>,
     pub unnamed: Vec<Clip>,
+}
+
+impl Clip {
+    pub fn from_file<R: Read>(reader: R) -> Result<Clip, String> {
+        let bytes: Result<Vec<_>, _> = reader.bytes().collect();
+        let bytes = bytes.map_err(|_| "Error reading bytes from file")?;
+        let s: String = std::str::from_utf8(&bytes[..bytes.len() - 1])
+            .map_err(|_| "Error converting file content into string")?
+            .to_string();
+        Ok(Clip::from(s))
+    }
+}
+
+impl From<String> for Clip {
+    fn from(value: String) -> Clip {
+        return Clip(value);
+    }
 }
 
 impl Clips {
@@ -38,18 +58,18 @@ impl Clips {
         }
     }
 
-    fn write(&self) -> Result<(), String> {
+    pub fn write(&self) -> Result<(), String> {
         let cache = Clips::get_dir();
         if !cache.parent().unwrap().exists() {
             fs::create_dir(cache.parent().unwrap())
-                .or_else(|_| Err("Could not create cache directory"))?;
+                .map_err(|_| "Could not create cache directory")?;
         }
-        let res = serde_json::to_string(self).or_else(|_| Err("Could not serialize Clips"))?;
-        fs::write(cache, res).or_else(|_| Err("Could not write to cache file"))?;
+        let res = serde_json::to_string(self).map_err(|_| "Could not serialize Clips")?;
+        fs::write(cache, res).map_err(|_| "Could not write to cache file")?;
         Ok(())
     }
 
-    pub fn add(mut self, key: Option<String>, clip: Clip) -> Result<(), String> {
+    pub fn add(&mut self, key: Option<ClipKey>, clip: Clip) {
         if let Some(key) = key {
             self.named.insert(key, clip);
         } else {
@@ -57,6 +77,37 @@ impl Clips {
                 self.unnamed.push(clip);
             }
         }
-        self.write()
+    }
+
+    pub fn clear(&mut self, key: Option<ClipKey>, unnamed_only: bool) -> Result<(), String> {
+        if let Some(key) = key {
+            self.named
+                .remove(&key)
+                .ok_or(format!("Key {} did not exist.", key))?;
+        } else if unnamed_only {
+            self.unnamed.clear();
+        } else {
+            self.named.clear();
+            self.unnamed.clear();
+        }
+        Ok(())
+    }
+
+    pub fn print(&self, key: Option<ClipKey>) -> Result<(), String> {
+        if let Some(key) = key {
+            let clip = self
+                .named
+                .get(&key)
+                .ok_or(format!("Key {} does not exist", key))?;
+            println!("{}:\t{}", key, clip.0);
+        } else {
+            self.named.iter().for_each(|clip| {
+                println!("{}:\t{}", clip.0, clip.1.0);
+            });
+            self.unnamed.iter().for_each(|clip| {
+                println!("\t{}", clip.0);
+            });
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,82 @@
 mod clip;
-use clip::{Clip, Clips};
+use clip::{Clip, ClipKey, Clips};
+use std::path::PathBuf;
 use structopt::StructOpt;
+use std::fs;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "karl: CLI clipboard manager")]
-enum Karl {
+enum KarlArgs {
     Add {
         #[structopt(short, long, help = "Key used for quick access, can be ommited")]
-        key: Option<String>,
-        #[structopt(help = "Actual data of clipboard entry")]
-        value: String,
+        key: Option<ClipKey>,
+
+        #[structopt(
+            short,
+            long,
+            help = "Read from file",
+            conflicts_with = "value",
+            required_unless = "value"
+        )]
+        file: Option<Option<PathBuf>>,
+
+        #[structopt(
+            help = "Data of clipboard entry",
+            conflicts_with = "file",
+            required_unless = "file"
+        )]
+        value: Option<String>,
     },
     Clear {
-        #[structopt(short, long, help = "Set if only unnamed entries should be cleared")]
+        #[structopt(short, long, help = "Delete only key", conflicts_with = "unnamed_only")]
+        key: Option<ClipKey>,
+        #[structopt(
+            short,
+            long,
+            help = "Set if only unnamed entries should be cleared",
+            conflicts_with = "key"
+        )]
         unnamed_only: bool,
     },
-    List,
+    List {
+        #[structopt(short, long, help = "List only key")]
+        key: Option<ClipKey>,
+    },
+}
+
+
+fn handle_args(args: KarlArgs) -> Result<(), String> {
+    match args {
+        KarlArgs::Add { key, file, value } => {
+            let mut clips = Clips::read();
+            if let Some(value) = value {
+                clips.add(key, Clip::from(value));
+            } else if let Some(file) = file {
+                let clip = match file {
+                    Some(f) => Clip::from_file(fs::File::open(f).map_err(|_| "Fuck")?),
+                    None => Clip::from_file(std::io::stdin()),
+                }?;
+                clips.add(key, clip);
+            }
+            clips.write()?;
+
+        }
+        KarlArgs::Clear { key, unnamed_only } => {
+            let mut clips = Clips::read();
+            clips.clear(key, unnamed_only)?;
+            clips.write()?;
+        }
+        KarlArgs::List { key } => {
+            let clips = Clips::read();
+            clips.print(key)?;
+        }
+    }
+    Ok(())
 }
 
 fn main() {
-    let args = Karl::from_args();
-    match args {
-        Karl::Add { key, value } => {
-            let clips = Clips::read();
-            clips.add(key, Clip(value)).unwrap();
-        }
-        Karl::Clear { unnamed_only } => {}
-        Karl::List => {
-            let clips = Clips::read();
-            clips.named.iter().for_each(|clip| {
-                println!("{:?}, {:?}", clip.0, clip.1);
-            });
-            clips.unnamed.iter().for_each(|clip| {
-                println!("{:?}", clip);
-            });
-        }
+    let args = KarlArgs::from_args();
+    if let Err(e) = handle_args(args) {
+        eprintln!("Error: {}", e);
     }
 }
