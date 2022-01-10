9332c96710212fa2c4fe4a5f983cd49221a0df8d
Lars Haalck <lars.haalck@uni-muenster.de>
2022-01-10 11:06:48 +0100

    Add copy custom action to rofi

--- scripts/karl_rofi.sh
+++ scripts/karl_rofi.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
-cache_file="${XDG_CACHE_HOME-$HOME/.cache}/karl/clips.data"
-output=$(cat "${cache_file}" | jq ".named[], .unnamed[]" | rofi -dmenu -p "Karl")
+KB_COPY="Alt+c"
+CLIP_COMMAND="xclip -selection clipboard"
+
+_rofi() {
+    rofi -dmenu -p "Karl" \
+        -kb-custom-1 "${KB_COPY}"
+}
+
+
+output=$(karl list --rofi | _rofi)
+val=$?
+
+# remove key
+output=$(echo "${output}" | cut -d',' -f 2)
+# remove outer quotation marks and one level of inner quotations
 output=$(echo "${output:1:-1}" | sed 's/\\"/"/g')
+# interpret backlash escapes
 output=$(echo -e "${output}")
-xdotool type "${output}"
+
+if [[ $val -eq 10 ]]; then
+    echo "${output}" | eval "${CLIP_COMMAND}"
+else
+    xdotool type "${output}"
+fi
